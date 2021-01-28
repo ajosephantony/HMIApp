167cd9f11b077189829cc04fa504811d91ef7c2f
joseph <joseph.antony@jasmin-infotech.com>
2021-01-28 12:42:56 +0530

    updated c code

--- thirdworld.c
+++ thirdworld.c
@@ -1,9 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#define STATUS_SUCCESS 0
+
 int main(int argc, char *argv)
 {
 	// Adding new line of comment
+	// adding 2nd line of comment for new commit.
 	printf ("hello world\n");
-	return 0;
+	return STATUS_SUCCESS;
 }
