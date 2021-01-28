5a38eff8f1d74a83916cee6ea48f5514c3b1d0d6
joseph <joseph.antony@jasmin-infotech.com>
2021-01-28 12:50:57 +0530

    code update 2

--- thirdworld.c
+++ thirdworld.c
@@ -5,8 +5,16 @@
 
 int main(int argc, char *argv)
 {
+	int ncount = 0;
+	int i;
+	
+	for (i = 0; i < 5; i++) {
+		printf (" %d \n", i);
+	}
+	
 	// Adding new line of comment
 	// adding 2nd line of comment for new commit.
 	printf ("hello world\n");
+	
 	return STATUS_SUCCESS;
 }
