2c685068a2abed0fe800359f7d80d6a9e134fcae
joseph <joseph.antony@jasmin-infotech.com>
2021-01-29 16:11:45 +0530

    updated code

--- SWE3/src/source1.c
+++ SWE3/src/source1.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+#include "source1.h"
 
 int main ()
 {
---
+++ SWE3/src/source1.h
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#ifndef TRUE
+	#define TRUE 1
+#endif
+
+#ifndef FALSE
+	#define FALSE 0
+#endif
+
